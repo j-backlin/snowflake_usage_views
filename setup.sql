51d4ddd588bc0f733fad186c03622b950bcd2987
Johan <baecklin@gmail.com>
2025-07-17 11:54:10 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -2,7 +2,7 @@
 USE ROLE ACCOUNTADMIN;
 GRANT READ SESSION ON ACCOUNT TO ROLE ACCOUNTADMIN;
 
-create database snowflake_copy_cost_views;
+--create database snowflake_copy_cost_views;
 create schema snowflake_copy_cost_views.account_usage;
 create schema snowflake_copy_cost_views.policies;
 create schema snowflake_copy_cost_views.streamlit;
