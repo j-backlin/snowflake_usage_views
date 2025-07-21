1181962ca7c32695066ee7687aef2131c0f3ac0e
Johan <baecklin@gmail.com>
2025-07-21 11:42:14 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -17,8 +17,7 @@ SELECT * FROM snowflake.account_usage.CORTEX_ANALYST_USAGE_HISTORY;
 ALTER TABLE snowflake_copy_cost_views.account_usage.CORTEX_ANALYST_USAGE_HISTORY ADD ROW ACCESS POLICY snowflake_copy_cost_views.policies.user_row_access_policy ON (username);
 
 CREATE OR REPLACE SECURE VIEW snowflake_copy_cost_views.account_usage.query_history AS
-SELECT *,'https://app.snowflake.com/'||CURRENT_ORGANIZATION_NAME()||'/'||CURRENT_ACCOUNT_NAME()||'/#/compute/history/queries/'||QUERY_ID||'/profile' as query_id_url_org,
-    'https://app.snowflake.com/'||CURRENT_REGION()||'/'||CURRENT_ACCOUNT()||'/#/compute/history/queries/'||QUERY_ID||'/profile' as query_id_url_reg, FROM snowflake.account_usage.query_history;
+SELECT *,$base_url||'/#/compute/history/queries/'||QUERY_ID||'/profile' as query_id_url, FROM snowflake.account_usage.query_history;
 ALTER TABLE snowflake_copy_cost_views.account_usage.query_history ADD ROW ACCESS POLICY snowflake_copy_cost_views.policies.user_row_access_policy ON (user_name);
 
 CREATE OR REPLACE SECURE VIEW snowflake_copy_cost_views.account_usage.cortex_functions_query_usage_history AS (SELECT t1.*, t2.* exclude (query_id, warehouse_id) FROM snowflake_copy_cost_views.account_usage.query_history t1
@@ -29,9 +28,9 @@ CREATE OR REPLACE SECURE VIEW snowflake_copy_cost_views.account_usage.query_attr
 SELECT * FROM snowflake.account_usage.query_attribution_history;
 ALTER TABLE snowflake_copy_cost_views.account_usage.query_attribution_history ADD ROW ACCESS POLICY snowflake_copy_cost_views.policies.user_row_access_policy ON (user_name);
 
-grant usage on database snowflake_copy_cost_views to role public;
-grant usage on schema snowflake_copy_cost_views.streamlit to role public;
-grant select on view snowflake_copy_cost_views.account_usage.CORTEX_ANALYST_USAGE_HISTORY to role public;
-grant select on view snowflake_copy_cost_views.account_usage.query_history to role public;
-grant select on view snowflake_copy_cost_views.account_usage.cortex_functions_query_usage_history to role public;
-grant select on view snowflake_copy_cost_views.account_usage.query_attribution_history to role public;
+grant usage on database snowflake_copy_cost_views to role identifier($role_access);
+grant usage on schema snowflake_copy_cost_views.streamlit to role identifier($role_access);
+grant select on view snowflake_copy_cost_views.account_usage.CORTEX_ANALYST_USAGE_HISTORY to role identifier($role_access);
+grant select on view snowflake_copy_cost_views.account_usage.query_history to role identifier($role_access);
+grant select on view snowflake_copy_cost_views.account_usage.cortex_functions_query_usage_history to role identifier($role_access);
+grant select on view snowflake_copy_cost_views.account_usage.query_attribution_history to role identifier($role_access);
