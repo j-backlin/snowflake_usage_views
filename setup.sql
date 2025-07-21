8ec71c39fce0e29b36c7b0c4ed66c84264fc5361
Johan <baecklin@gmail.com>
2025-07-21 11:54:15 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,9 +1,5 @@
--- drop database snowflake_copy_cost_views;
-USE ROLE ACCOUNTADMIN;
 GRANT READ SESSION ON ACCOUNT TO ROLE ACCOUNTADMIN;
 
---create database snowflake_copy_cost_views;
-use database snowflake_copy_cost_views;
 create schema snowflake_copy_cost_views.account_usage;
 create schema snowflake_copy_cost_views.policies;
 create schema snowflake_copy_cost_views.streamlit;
@@ -16,8 +12,8 @@ CREATE OR REPLACE SECURE VIEW snowflake_copy_cost_views.account_usage.CORTEX_ANA
 SELECT * FROM snowflake.account_usage.CORTEX_ANALYST_USAGE_HISTORY;
 ALTER TABLE snowflake_copy_cost_views.account_usage.CORTEX_ANALYST_USAGE_HISTORY ADD ROW ACCESS POLICY snowflake_copy_cost_views.policies.user_row_access_policy ON (username);
 
-CREATE OR REPLACE SECURE VIEW snowflake_copy_cost_views.account_usage.query_history AS
-SELECT *,$base_url||'/#/compute/history/queries/'||QUERY_ID||'/profile' as query_id_url, FROM snowflake.account_usage.query_history;
+/*CREATE OR REPLACE SECURE VIEW snowflake_copy_cost_views.account_usage.query_history AS
+SELECT *,$base_url||'/#/compute/history/queries/'||QUERY_ID||'/profile' as query_id_url, FROM snowflake.account_usage.query_history;*/
 ALTER TABLE snowflake_copy_cost_views.account_usage.query_history ADD ROW ACCESS POLICY snowflake_copy_cost_views.policies.user_row_access_policy ON (user_name);
 
 CREATE OR REPLACE SECURE VIEW snowflake_copy_cost_views.account_usage.cortex_functions_query_usage_history AS (SELECT t1.*, t2.* exclude (query_id, warehouse_id) FROM snowflake_copy_cost_views.account_usage.query_history t1
